f7e26b848ba5e64108eeca92e672c69d2fcf4880
Wojciech Szostak <gwszostak@gmail.com>
2014-01-15 12:00:02 +0100

    Example transfers

--- src/main/java/pl/training/bank/App.java
+++ src/main/java/pl/training/bank/App.java
@@ -2,7 +2,10 @@ package pl.training.bank;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pl.training.bank.entity.Account;
 import pl.training.bank.entity.Client;
+
+import java.math.BigDecimal;
 
 public class App
 {
@@ -10,9 +13,16 @@ public class App
         ApplicationContext context = new ClassPathXmlApplicationContext("bank.xml");
         Bank bank = context.getBean(Bank.class);
         try {
-            bank.addClient(new Client());
+            Account account1 = bank.addAccount(new Account());
+            Account account2 = bank.addAccount(new Account());
+
+            bank.payInCashToAccount(account1.getNumber(), new BigDecimal(1000));
+            bank.payInCashToAccount(account2.getNumber(), new BigDecimal(200));
+            bank.transferCash(account1.getNumber(), account2.getNumber(), new BigDecimal(10));
+            bank.payInCashToAccount("0001", new BigDecimal(0));
+
         } catch (BankException e) {
-            e.printStackTrace();
+            // e.printStackTrace();
         }
     }
 
