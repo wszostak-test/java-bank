aef891b2a1a4739c2266a9f0fc46dec1fcb1ebda
Wojciech Szostak <gwszostak@gmail.com>
2014-01-15 12:27:23 +0100

    Changed aspects configuration from annotations to xml

--- src/main/java/pl/training/bank/aspect/CashFlowLogger.java
+++ src/main/java/pl/training/bank/aspect/CashFlowLogger.java
@@ -9,24 +9,24 @@ import java.math.BigDecimal;
 import java.text.NumberFormat;
 
 @Component
-@Aspect
+// @Aspect
 public class CashFlowLogger
 {
-    @Pointcut("execution(* pl.training.bank.Bank.payInCashToAccount(..)) && args(accountNumber, amount)")
-    public void payIn(String accountNumber, BigDecimal amount) {
-    }
-
-    @Pointcut("execution(* pl.training.bank.Bank.payOutCashToAccount(..)) && args(accountNumber, amount)")
-    public void payOut(String accountNumber, BigDecimal amount) {
-    }
-
-    @Pointcut("execution(* pl.training.bank.Bank.transferCash(..)) && args(fromAccountNumber, toAccountNumber, amount)")
-    public void transferCash(String fromAccountNumber, String toAccountNumber, BigDecimal amount) {
-    }
-
-    @Pointcut("execution (* pl.training.bank.Bank.*Cash*(..))")
-    public void cashFlowOperation() {
-    }
+//    @Pointcut("execution(* pl.training.bank.Bank.payInCashToAccount(..)) && args(accountNumber, amount)")
+//    public void payIn(String accountNumber, BigDecimal amount) {
+//    }
+//
+//    @Pointcut("execution(* pl.training.bank.Bank.payOutCashToAccount(..)) && args(accountNumber, amount)")
+//    public void payOut(String accountNumber, BigDecimal amount) {
+//    }
+//
+//    @Pointcut("execution(* pl.training.bank.Bank.transferCash(..)) && args(fromAccountNumber, toAccountNumber, amount)")
+//    public void transferCash(String fromAccountNumber, String toAccountNumber, BigDecimal amount) {
+//    }
+//
+//    @Pointcut("execution (* pl.training.bank.Bank.*Cash*(..))")
+//    public void cashFlowOperation() {
+//    }
 
     private void startLogEntry(String operationName) {
         System.out.println("=========================================");
@@ -37,33 +37,33 @@ public class CashFlowLogger
         return NumberFormat.getCurrencyInstance().format(amount);
     }
 
-    @Before("payIn(accountNumber, amount)")
+    // @Before("payIn(accountNumber, amount)")
     public void beforePayInOperation(JoinPoint jp, String accountNumber, BigDecimal amount) {
         startLogEntry(jp.getSignature().getName());
         System.out.println(formatCurrency(amount) + " -> " + accountNumber);
     }
 
-    @Before("payOut(accountNumber, amount)")
+    // @Before("payOut(accountNumber, amount)")
     public void beforePayOutOperation(JoinPoint jp, String accountNumber, BigDecimal amount) {
         startLogEntry(jp.getSignature().getName());
         System.out.println(accountNumber + " -> " + formatCurrency(amount));
     }
 
-    @Before("transferCash(fromAccountNumber, toAccountNumber, amount)")
-    public void beforePayOutOperation(JoinPoint jp, String fromAccountNumber, String toAccountNumber, BigDecimal amount) {
+    // @Before("transferCash(fromAccountNumber, toAccountNumber, amount)")
+    public void beforeTransferCashOperation(JoinPoint jp, String fromAccountNumber, String toAccountNumber, BigDecimal amount) {
         startLogEntry(jp.getSignature().getName());
         System.out.println(fromAccountNumber
                 + " -> " + formatCurrency(amount)+ " -> "
                 + toAccountNumber);
     }
 
-    @AfterReturning("cashFlowOperation()")
+    // @AfterReturning("cashFlowOperation()")
     public void afterSuccess() {
         System.out.println("Operacja zakończona pomyślnie");
         System.out.println("=========================================");
     }
 
-    @AfterThrowing(value="cashFlowOperation()", throwing = "bankException")
+    // @AfterThrowing(value="cashFlowOperation()", throwing = "bankException")
     public void afterFailure(BankException bankException) {
         System.out.println("Wyjątek: " + bankException.getClass().getSimpleName());
         System.out.println("=========================================");
