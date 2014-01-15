769e56700c6b77f7bae52f66715900b50ab565e9
Wojciech Szostak <gwszostak@gmail.com>
2014-01-15 15:30:03 +0100

    Some Jdbc implementations

--- src/main/java/pl/training/bank/App.java
+++ src/main/java/pl/training/bank/App.java
@@ -4,30 +4,28 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import pl.training.bank.entity.Account;
 import pl.training.bank.entity.Client;
+import pl.training.bank.service.JdbcAccountNumberGenerator;
 
+import javax.sql.DataSource;
 import java.math.BigDecimal;
 
 public class App
 {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("bank.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                "bank.xml",
+                "bank-repository.xml");
+
         Bank bank = context.getBean(Bank.class);
+
         try {
             Account account1 = bank.addAccount(new Account());
-            Account account2 = bank.addAccount(new Account());
-            System.out.println("Account 1 balance: " + account1.getBalance());
-            System.out.println("Account 2 balance: " + account2.getBalance());
 
-            bank.payInCashToAccount(account1.getNumber(), new BigDecimal(1000));
-            bank.payInCashToAccount(account2.getNumber(), new BigDecimal(200));
-            System.out.println("Account 1 balance: " + account1.getBalance());
-            System.out.println("Account 2 balance: " + account2.getBalance());
+            Client client = new Client();
+            client.setFirstName("Jan");
+            client.setLastName("Kolwalski");
 
-            bank.transferCash(account1.getNumber(), account2.getNumber(), new BigDecimal(10));
-            System.out.println("Account 1 balance: " + account1.getBalance());
-            System.out.println("Account 2 balance: " + account2.getBalance());
-
-            bank.payInCashToAccount("0001", new BigDecimal(0));
+            bank.addClient(client);
         } catch (BankException e) {
             // e.printStackTrace();
         }
---
+++ src/main/java/pl/training/bank/service/repository/JdbcClientRepository.java
@@ -0,0 +1,72 @@
+package pl.training.bank.service.repository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+import pl.training.bank.entity.Client;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
+public class JdbcClientRepository implements ClientRepository
+{
+    private static final String SQL_INSERT_CLIENT =
+        "insert into client (id, firstName, lastName) values (null, :firstName, :lastName)";
+    private static final String SQL_SELECT_CLIENT =
+        "select * from client c where c.id = :id";
+    private NamedParameterJdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public JdbcClientRepository(DataSource dataSource) {
+        jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Override
+    public Client add(Client client) {
+        KeyHolder key = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(
+            SQL_INSERT_CLIENT,
+            new BeanPropertySqlParameterSource(client),
+            key
+        );
+
+        client.setId(key.getKey().longValue());
+
+        return client;
+    }
+
+    @Override
+    public Client get(Long id) {
+        return jdbcTemplate.query(
+            SQL_SELECT_CLIENT,
+            new MapSqlParameterSource("id", id),
+            new ClientMapper()
+        );
+    }
+
+    private class ClientMapper implements ResultSetExtractor<Client> {
+        @Override
+        public Client extractData(ResultSet resultSet) throws SQLException, DataAccessException {
+            Client client = null;
+
+            if (resultSet.next()) {
+                client = new Client();
+                client.setId(resultSet.getLong("id"));
+                client.setFirstName(resultSet.getString("firstName"));
+                client.setLastName(resultSet.getString("lastName"));
+            }
+
+            return client;
+        }
+    }
+}
